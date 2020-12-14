c7589b2b3b601927fa1919c9f903e77f7951c84b
ayakanishimura <a.tktk08@gmail.com>
2020-12-14 14:09:04 +0000

    device作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201213074019) do
+ActiveRecord::Schema.define(version: 20201214094828) do
 
+  create_table "customers", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "first_name_kana"
+    t.string   "last_name_kana"
+    t.string   "postcode"
+    t.string   "address"
+    t.string   "phone_number"
+    t.boolean  "user_status",            default: false, null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
 
 end
