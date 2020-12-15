76e4717461c89baf65214e84460325e05cbadd9f
EC2 Default User <ec2-user@ip-172-31-13-202.ec2.internal>
2020-12-15 07:41:26 +0000

    [Update]配送先住所登録機能の実装

---
+++ app/controllers/shipping_addresses_controller.rb
@@ -0,0 +1,50 @@
+class ShippingAddressesController < ApplicationController
+    before_action :authenticate_customer!
+
+  def index
+	@shipping_addresses = ShippingAddress.all
+  	@shipping_address = ShippingAddress.new
+  end
+
+ def create
+	  @shipping_address = ShippingAddress.new(shipping_address_params)
+	  @shipping_address.customer_id = current_customer.id
+	  if @shipping_address.save
+	  	 flash.now[:notice] = "新規配送先を登録しました"
+	      redirect_to shipping_addresses_path
+	  else
+	   	# @shipping_addresses = current_customer.shipping_address
+	      render 'index'
+	  end
+ end
+
+ def edit
+     @shipping_address = ShippingAddress.find(params[:id])
+ end
+
+ def update
+	  @shipping_address = ShippingAddress.find(params[:id])
+	  if @shipping_address.update(shipping_address_params)
+  	   flash[:success] = "配送先を変更しました"
+       redirect_to shipping_addresses_path
+	  else
+	   render "edit"
+	  end
+ end
+
+ def destroy
+	  @shipping_address = ShippingAddress.find(params[:id])
+	  if @shipping_address.destroy
+      #@shipping_addresses = current_customer.shipping_address
+      flash.now[:alert] = "配送先を削除しました"
+	  redirect_to shipping_addresses_path
+	  end
+ end
+ 
+
+private
+
+    def shipping_address_params
+  	params.require(:shipping_address).permit(:postcode, :address, :name)
+    end
+end
